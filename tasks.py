4b16d08c28aa4df2e9446a2e7699098b45bd04b9
toby cabot <toby@caboteria.org>
2020-06-24 17:40:11 -0400

    Delete stuff that's not controller related
    The goal of this POC is to build a controller that works with MetalLB
    and fetches IP addresses from Netbox, so we don't need to build
    speakers or a website - just a controller.

--- tasks.py
+++ tasks.py
@@ -12,9 +12,7 @@ except ImportError:
 from invoke import run, task
 from invoke.exceptions import Exit
 
-all_binaries = set(["controller",
-                    "speaker",
-                    "mirror-server"])
+all_binaries = set(["controller"])
 all_architectures = set(["amd64",
                          "arm",
                          "arm64",
@@ -49,7 +47,6 @@ def _check_binaries(binaries):
             out.add(binary)
     if not out:
         out.add("controller")
-        out.add("speaker")
     return list(sorted(out))
 
 def _make_build_dirs():
@@ -150,114 +147,6 @@ def push_multiarch(ctx, binaries, tag="dev", docker_user="metallb"):
             echo=True)
 
 @task(help={
-    "architecture": "CPU architecture of the local machine. Default 'amd64'.",
-    "name": "name of the kind cluster to use.",
-})
-def dev_env(ctx, architecture="amd64", name="kind", cni=None):
-    """Build and run MetalLB in a local Kind cluster.
-
-    If the cluster specified by --name (default "kind") doesn't exist,
-    it is created. Then, build MetalLB docker images from the
-    checkout, push them into kind, and deploy manifests/metallb.yaml
-    to run those images.
-    """
-    clusters = run("kind get clusters", hide=True).stdout.strip().splitlines()
-    mk_cluster = name not in clusters
-    if mk_cluster:
-        config = {
-            "apiVersion": "kind.sigs.k8s.io/v1alpha3",
-            "kind": "Cluster",
-            "nodes": [{"role": "control-plane"},
-                      {"role": "worker"},
-                      {"role": "worker"},
-            ],
-        }
-        if cni:
-            config["networking"] = {
-                "disableDefaultCNI": True,
-            }
-        config = yaml.dump(config).encode("utf-8")
-        with tempfile.NamedTemporaryFile() as tmp:
-            tmp.write(config)
-            tmp.flush()
-            run("kind create cluster --name={} --config={}".format(name, tmp.name), pty=True, echo=True)
-
-    if mk_cluster and cni:
-        run("kubectl apply -f e2etest/manifests/{}.yaml".format(cni), echo=True)
-
-    build(ctx, binaries=["controller", "speaker", "mirror-server"], architectures=[architecture])
-    run("kind load docker-image --name={} metallb/controller:dev-{}".format(name, architecture), echo=True)
-    run("kind load docker-image --name={} metallb/speaker:dev-{}".format(name, architecture), echo=True)
-    run("kind load docker-image --name={} metallb/mirror-server:dev-{}".format(name, architecture), echo=True)
-
-    run("kubectl delete po -nmetallb-system --all", echo=True)
-
-    manifests_dir = os.getcwd() + "/manifests"
-    with tempfile.TemporaryDirectory() as tmpdir:
-        # Copy namespace manifest.
-        shutil.copy(manifests_dir + "/namespace.yaml", tmpdir)
-
-        with open(manifests_dir + "/metallb.yaml") as f:
-            manifest = f.read()
-        manifest = manifest.replace(":main", ":dev-{}".format(architecture))
-        manifest = manifest.replace("imagePullPolicy: Always", "imagePullPolicy: Never")
-        with open(tmpdir + "/metallb.yaml", "w") as f:
-            f.write(manifest)
-            f.flush()
-
-        # Create memberlist secret.
-        secret = """---
-apiVersion: v1
-kind: Secret
-metadata:
-  name: memberlist
-  namespace: metallb-system
-stringData:
-  secretkey: verysecurelol"""
-
-        with open(tmpdir + "/secret.yaml", "w") as f:
-            f.write(secret)
-            f.flush()
-
-        run("kubectl apply -f {}/namespace.yaml".format(tmpdir), echo=True)
-        run("kubectl apply -f {}/secret.yaml".format(tmpdir), echo=True)
-        run("kubectl apply -f {}/metallb.yaml".format(tmpdir), echo=True)
-
-    with open("e2etest/manifests/mirror-server.yaml") as f:
-        manifest = f.read()
-    manifest = manifest.replace(":main", ":dev-{}".format(architecture))
-    with tempfile.NamedTemporaryFile() as tmp:
-        tmp.write(manifest.encode("utf-8"))
-        tmp.flush()
-        run("kubectl apply -f {}".format(tmp.name), echo=True)
-
-@task
-def test_cni_manifests(ctx):
-    """Update CNI manifests for e2e tests."""
-    def _fetch(url):
-        bs = urlopen(url).read()
-        return list(m for m in yaml.safe_load_all(bs) if m)
-    def _write(file, manifest):
-        with open(file, "w") as f:
-            f.write(yaml.dump_all(manifest))
-
-    calico = _fetch("https://docs.projectcalico.org/v3.6/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml")
-    for manifest in calico:
-        if manifest["kind"] != "DaemonSet":
-            continue
-        manifest["spec"]["template"]["spec"]["containers"][0]["env"].append({
-            "name": "FELIX_IGNORELOOSERPF",
-            "value": "true",
-        })
-    _write("e2etest/manifests/calico.yaml", calico)
-
-    weave = _fetch("https://cloud.weave.works/k8s/net?k8s-version=1.15&env.NO_MASQ_LOCAL=1")
-    _write("e2etest/manifests/weave.yaml", weave)
-
-    flannel = _fetch("https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml")
-    _write("e2etest/manifests/flannel.yaml", flannel)
-
-@task(help={
     "version": "version of MetalLB to release.",
     "skip-release-notes": "make the release even if there are no release notes.",
 })
@@ -270,13 +159,6 @@ def release(ctx, version, skip_release_notes=False):
     version = semver.parse_version_info(version)
     is_patch_release = version.patch != 0
 
-    # Check that we have release notes for the desired version.
-    run("git checkout main", echo=True)
-    if not skip_release_notes:
-        with open("website/content/release-notes/_index.md") as release_notes:
-            if "## Version {}".format(version) not in release_notes.read():
-                raise Exit(message="no release notes for v{}".format(version))
-
     # Move HEAD to the correct release branch - either a new one, or
     # an existing one.
     if is_patch_release:
@@ -284,30 +166,7 @@ def release(ctx, version, skip_release_notes=False):
     else:
         run("git checkout -b v{}.{}".format(version.major, version.minor), echo=True)
 
-    # Copy over release notes from main.
-    if not skip_release_notes:
-        run("git checkout main -- website/content/release-notes/_index.md", echo=True)
-
-    # Update links on the website to point to files at the version
-    # we're creating.
-    if is_patch_release:
-        previous_version = "v{}.{}.{}".format(version.major, version.minor, version.patch-1)
-    else:
-        previous_version = "main"
-    def _replace(pattern):
-        oldpat = pattern.format(previous_version)
-        newpat = pattern.format("v{}").format(version)
-        run("perl -pi -e 's#{}#{}#g' website/content/*.md website/content/*/*.md".format(oldpat, newpat),
-            echo=True)
-    _replace("/google/metallb/{}")
-    _replace("/google/metallb/tree/{}")
-    _replace("/google/metallb/blob/{}")
-
-    # Update the version listed on the website sidebar
-    run("perl -pi -e 's/MetalLB .*/MetalLB v{}/g' website/content/_header.md".format(version), echo=True)
-
     # Update the manifests with the new version
-    run("perl -pi -e 's,image: metallb/speaker:.*,image: metallb/speaker:v{},g' manifests/metallb.yaml".format(version), echo=True)
     run("perl -pi -e 's,image: metallb/controller:.*,image: metallb/controller:v{},g' manifests/metallb.yaml".format(version), echo=True)
 
     # Update the version embedded in the binary
@@ -315,5 +174,4 @@ def release(ctx, version, skip_release_notes=False):
     run("gofmt -w internal/version/version.go", echo=True)
 
     run("git commit -a -m 'Automated update for release v{}'".format(version), echo=True)
-    run("git tag v{} -m 'See the release notes for details:\n\nhttps://metallb.universe.tf/release-notes/#version-{}-{}-{}'".format(version, version.major, version.minor, version.patch), echo=True)
     run("git checkout main", echo=True)
